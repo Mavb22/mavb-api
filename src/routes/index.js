e2eed3ffd2fca8919b026247bcad25815e804ce5
Mavb22 <mavb.app@gmail.com>
2022-10-08 18:26:06 -0500

    configuraciones

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+// import routerUser  from "./user.routes.js";
+const router = Router();
+router.get("/", (req, res) => {
+    res.send("Hello World! api");
+});
+// router.use("/users", routerUser);
+export default router;
