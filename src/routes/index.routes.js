e19c151d302c3709f190b3d11af6b111b0ce6879
Mavb22 <mavb.app@gmail.com>
2022-10-09 17:51:10 -0500

    Update index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import routerUser  from "./user.routes.js";
 const router = Router();
 router.get("/", (req, res) => {
-    res.send("Hello World! api" + process.env.APP_NAME);
+    res.send("Hello World!" + process.env.APP_NAME);
 });
 router.use("/users", routerUser);
 export default router;
