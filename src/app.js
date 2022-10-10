8912e1699fda56fdc989959868189ff54f7cd324
Mavb22 <mavb.app@gmail.com>
2022-10-09 21:25:45 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import router from './routes/index.routes.js';
-import SMTPPool from "nodemailer/lib/smtp-pool/index.js";
 const app = express();
 const corsOptions = {
     origin: "http://localhost:3500",
@@ -21,7 +20,7 @@ app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use("/upload", express.static("upload"));
 app.get("/", (req, res) => {
-    res.json({msj:"Hello World! " + process.env.APP_NAME});
+    res.send("Hello World!" + process.env.APP_NAME);
 })
 app.use("/api",router);
 export default app;
